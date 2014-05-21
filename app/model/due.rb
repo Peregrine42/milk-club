0fc870d437bca7c4d2db89bedaf5e72012bb825d
Duncan Fermor <duncan.fermor@bt.com>
2014-05-21 15:14:34 +0100

    removing old model folder

--- app/model/due.rb
+++
@@ -1,9 +0,0 @@
-class Due < ActiveRecord::Base
-  belongs_to :member
-
-  def self.for_members amount, members
-    members.each do |member|
-      Due.create!(amount: amount, member_id: member.id)
-    end
-  end
-end
--- app/model/fee.rb
+++
@@ -1,7 +0,0 @@
-class Fee < ActiveRecord::Base
-  def self.most_recent
-    last_item = all.last
-    return 0 unless last_item
-    return last_item.amount
-  end
-end
