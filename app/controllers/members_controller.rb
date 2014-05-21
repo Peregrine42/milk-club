faebe38b09e7e331537ffcf3d538d071a7ed0bbd
Richard Pilkington <richard.pilkington@bt.com>
2014-05-21 17:36:55 +0100

    Members: show current member balance

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,5 @@
 class App < Sinatra::Base
   get '/members' do
-
+    erb :'members_page'
   end
 end
--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -4,7 +4,7 @@ describe "GET /members" do
 
   it "shows the user balance" do
     get_as_user '/members'
-    expect(last_response.body).to match("User User")
-    expect(last_response.body).to match("Balance: £3")
+    expect(last_response.body).to match("A User")
+    expect(last_response.body).to match("Balance: £0")
   end
 end
