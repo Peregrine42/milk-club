34e5dede89c89313b3b7970cd2d717685be8922f
Morgan Sadr-Hashemi <morgan.sadr@bt.com>
2014-07-14 10:48:59 +0100

    Totals: failing feature for total expenses page

--- features/step_definitions/total_expenses_page_steps.rb
+++ features/step_definitions/total_expenses_page_steps.rb
@@ -3,9 +3,9 @@ When  "I click the total expenses navbar link" do
 end
 
 Then  "I see the total balance for the month" do
-  pending
+  page.should have_content("2")
 end
 
 Then  "I see the expected balance for the month" do
-  pending
+  page.should have_content("3")
 end
