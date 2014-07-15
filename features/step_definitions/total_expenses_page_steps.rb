d63e47ccb3c6c4e261baac4e25f2f873d7b68fd5
Morgan Sadr-Hashemi <morgan.sadr@bt.com>
2014-07-15 11:21:34 +0100

    Added member info table to total expenses table

--- features/step_definitions/total_expenses_page_steps.rb
+++ features/step_definitions/total_expenses_page_steps.rb
@@ -3,9 +3,24 @@ When  "I click the total expenses navbar link" do
 end
 
 Then  "I see the total balance for the month" do
-  page.should have_content("2")
+  page.should have_content("30")
 end
 
 Then  "I see the expected balance for the month" do
-  page.should have_content("3")
+  page.should have_content("9")
+end
+
+Then  "I see the total due for the month for each user" do
+  within(".breakdown") do
+    page.should have_content("5")
+    page.should have_content("10")
+    page.should have_content("15")
+  end
+end
+
+Then  "I see the total balance for the month for each user" do
+  within(".breakdown") do
+    page.should have_content("3")
+    page.should have_content("6")
+  end
 end
--- features/total_expenses_page.feature
+++ features/total_expenses_page.feature
@@ -7,3 +7,11 @@ Feature: anything to do with the total expenses
       And  I click the total expenses navbar link
     Then  I see the total balance for the month
       And I see the expected balance for the month
+
+  Scenario:
+    Given I am logged in as an administrator
+      And members, payments and dues are in the database
+    When  I visit the home page
+      And  I click the total expenses navbar link
+    Then  I see the total due for the month for each user
+      And I see the total balance for the month for each user
